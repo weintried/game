969c38dfa99736fbbf41e5d4adc5113c519a4c4e
weinfailed <thefourthwein@gmail.com>
2024-12-22 11:50:40 +0800

    update CMake configuration and add Box2D library integration; enhance .gitignore for build artifacts

--- server/src/main.cpp
+++ server/src/main.cpp
@@ -1,14 +1,21 @@
 #include <iostream>
 using std::cout;
 
+#include <box2d/box2d.h>
+
 #include "server.hpp"
 
 int main() {
-    cout << "Starting server..." << std::endl;
+  cout << "Starting server..." << std::endl;
 
-    Server server;
-    server.start();
-    // ...existing code...
-    server.stop();
-    return 0;
+  b2Vec2 gravity(0.0f, -10.0f);
+  b2World world(gravity);
+
+  cout << gravity.x << " " << gravity.y << std::endl;
+
+  Server server;
+  server.start();
+  // ...existing code...
+  server.stop();
+  return 0;
 }
